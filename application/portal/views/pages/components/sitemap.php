b681909803813dbca5ba27a91952cf99f828a63b
ogawa takuya <ogawa@wiz-g.co.jp>
2013-06-06 13:21:58 +0900

    move redmine components.

---
+++ application/portal/views/pages/components/sitemap.php
@@ -0,0 +1,13 @@
+<!--==========================-->
+<!-- サイトマップ -->
+<!--==========================-->
+
+<div class="space_20"></div>
+<h3>Sitemap.</h3>
+<div id="improvement">
+<ol id="improvement">
+<?php echo output_contents($sitemap); ?>
+</ol>
+</div>
+<div class="space_10"></div>
+
